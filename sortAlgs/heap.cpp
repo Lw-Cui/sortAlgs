11f4d192ae963b6385a0a23beb61b02f365eb8ea
Lw-Cui <645739460@QQ.com>
2016-01-03 19:15:00 +0800

    add heap sort

---
+++ sortAlgs/heap.cpp
@@ -0,0 +1,35 @@
+#include "heap.hpp"
+using namespace std;
+
+int left(int pos) {
+    return pos * 2 + 1;
+}
+
+int right(int pos) {
+    return pos * 2 + 2;
+}
+
+void push_down(vector<int> &array, int end, int pos) {
+    while (left(pos) < end) {
+        if (right(pos) < end && array[left(pos)] < array[right(pos)] 
+                && array[pos] < array[right(pos)]) {
+            swap(array[right(pos)], array[pos]);
+            pos = right(pos);
+        } else if (array[pos] < array[left(pos)]) {
+            swap(array[left(pos)], array[pos]);
+            pos = left(pos);
+        } else {
+            break;
+        }
+    }
+}
+
+void sort(vector<int> &array) {
+    for (int i = array.size() / 2; i >= 0; i--)
+        push_down(array, array.size(), i);
+
+    for (int i = array.size() - 1; i > 0; i--) {
+        swap(array[i], array[0]);
+        push_down(array, i, 0);
+    }
+}
---
+++ sortAlgs/heap.hpp
@@ -0,0 +1,7 @@
+#ifndef _HEAD_H_
+#define _HEAD_H_
+#include <vector>
+extern "C" {
+    void sort(std::vector<int> &array);
+}
+#endif
