b5d42a627341f21f1b084751a7c135a689ec4aba
Lw-Cui <645739460@QQ.com>
2016-01-03 16:22:06 +0800

    add insertion sort

---
+++ sortAlgs/insertion.cpp
@@ -0,0 +1,15 @@
+#include "insertion.hpp"
+#include <iostream>
+using namespace std;
+
+void sort(vector<int> &array) {
+    for (int i = 1; i < array.size(); i++) {
+        int tmp = array[i];
+        int j = i - 1;
+        while (j >= 0 && array[j] > tmp) {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = tmp;
+    }
+}
---
+++ sortAlgs/insertion.hpp
@@ -0,0 +1,7 @@
+#ifndef _INSERTION_H_
+#define _INSERTION_H_
+#include <vector>
+extern "C" {
+    void sort(std::vector<int> &array);
+}
+#endif
