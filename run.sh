0e512f033545afccf031d962c6b47c224d060315
Lw-Cui <6457739460@QQ.com>
2016-01-06 22:48:09 +0800

    update merge.so

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 g++ SortCompare.cpp -ldl -g -o SortCompare.out
-./SortCompare.out insertion.so qsort.so heap.so bubble.so radix.so qsort3way.so
+./SortCompare.out insertion.so qsort.so heap.so bubble.so radix.so qsort3way.so merge.so
