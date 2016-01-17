213af8aa58d324a6c7f2a060ae80a493d73cb4de
Lw-Cui <6457739460@QQ.com>
2016-01-17 11:23:32 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 g++ SortCompare.cpp -ldl -g -o SortCompare.out
-./SortCompare.out insertion.so qsort.so heap.so bubble.so radix.so qsort3way.so merge.so
+./SortCompare.out \
+		insertion.so \
+		qsort.so\
+		heap.so\
+		bubble.so\
+		radix.so\
+		qsort3way.so\
+		merge.so\
+		shell.so
