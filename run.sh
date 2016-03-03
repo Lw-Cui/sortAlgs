58a97604dc5dd8969fd7804d6cdcd22c51e39fcf
lw <you@example.com>
2016-03-03 18:55:03 +0800

    using LD_LIBRARY_PATH to specify shared file

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-g++ SortCompare.cpp -ldl -g -o SortCompare.out
-./SortCompare.out \
-		-linsertion \
+LD_LIBRARY_PATH=lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
+g++ SortCompare.cpp -ldl -Wall -Wextra -O2 -g -o SortCompare.out
+./SortCompare.out\
+		-linsertion\
 		-lqsort\
 		-lheap\
 		-lbubble\
